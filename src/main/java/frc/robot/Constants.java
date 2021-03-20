92bb19edc05a8db4151c4369fa9e3879e165288d
Trevor Sears <trevorsears.main@gmail.com>
2021-03-20 13:23:22 -0400

    Updated shooter speed in zone 3 from 12K to 10.5K

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,28 +5,28 @@
 package frc.robot;
 
 public final class Constants {
-    
+
     // Speed and hardware constants
     public static final double
             driveSpeed = .08,
             turnSpeed = .05,
             maxWheelSpeed = .7,
-            
+
             maxTurnInputSpeed = 0.6,
             turnInputSpeedScalar = 1.5,
-            
+
             widthToHeightWheelbaseRatio = .851,
-            
+
             flyWheelSpeed = .75,
-            
+
             pulleySpeed = .35,
-            
+
             intakeSpeed = .45;
-    
+
     public static final double[] shooterSpeedModes = {
         12000,  // 1st zone
         9500,   // 2nd zone
-        12000,  // 3rd zone
+        10500,  // 3rd zone
         12000,  // 4th zone
     };
     
@@ -35,67 +35,67 @@ public final class Constants {
             wheelkP = .6,
             wheelkI = 0,
             wheelkD = 0,
-            
+
             // shooterkP = .0075,
             // shooterkI = .0001,
             // shooterkD = shooterkP * 5,
             // shooterkF = .09,
-            
+
             pulleykP = 0.05,
             pulleykI = 0.05,
             pulleykD = pulleykP * 5,
             pulleykF = 0.45;
-    
+
     // CAN IDs
     public static final int
             flSteerMotor = 12,
             flDriveMotor = 2,
             flSteerEncoder = 17,
-            
+
             frSteerMotor = 4,
             frDriveMotor = 3,
             frSteerEncoder = 19,
-            
+
             rlSteerMotor = 1,
             rlDriveMotor = 13,
             rlSteerEncoder = 16,
-            
+
             rrSteerMotor = 14,
             rrDriveMotor = 15,
             rrSteerEncoder = 18,
-            
+
             shooterLeft = 0,
             shooterRight = 5,
-            
+
             flyWheel = 8,
-            
+
             intake = 9,
-            
+
             pulley = 7;
-    
+
     // DIO
     public static final int
             bottomSensor = 0,
             middleSensor = 1,
             topSensor = 2;
-    
+
     // OI
     public static final int
             directMoveXAxis = 0,
             directMoveXAxisScalar = 1,
-            
+
             directMoveYAxis = 1,
             directMoveYAxisScalar = -1;
-    
+
     public static final int
             rotateXAxis = 4,
             rotateXAxisScalar = 1;
-    
+
     public static final double
             joystickDeadzone = .08;
-    
+
     public static final int
             driveController = 0,
             shootController = 1;
-    
+
 }
